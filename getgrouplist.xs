56878e67eba4c98295c88041097bd9e40ffd619b
Bastian Friedrich <bastian@bastian-friedrich.de>
2009-10-20 08:09:33 +0000

    Import of BASTIAN/User-getgrouplist-1.0 from CPAN.
    gitpan-cpan-distribution: User-getgrouplist
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         BASTIAN/User-getgrouplist-1.0.tar.bz2
    gitpan-cpan-author:       BASTIAN
    gitpan-cpan-maturity:     released

---
+++ getgrouplist.xs
@@ -0,0 +1,38 @@
+/*
+ * $Id: getgrouplist.xs,v 1.2 2009/10/20 08:06:40 bastian Exp $
+ *
+ * Copyright (C) 2007-2009 Collax GmbH
+ *                    (Bastian Friedrich <bastian.friedrich@collax.com>)
+ */
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+
+MODULE = User::getgrouplist PACKAGE = User::getgrouplist
+
+AV *
+getgrouplist(username)
+        const char *username
+    PREINIT:
+	int count = 0;
+	int i;
+	gid_t *groups = NULL;
+	struct passwd *pw;
+    PPCODE:
+	pw = getpwnam(username);
+	RETVAL = NULL;
+
+	if (pw != NULL) { /* Only execute when user exists */
+		if (getgrouplist(username, pw->pw_gid, NULL, &count) < 0) {
+			groups = (gid_t *) malloc(count * sizeof (gid_t));
+			getgrouplist(username, pw->pw_gid, groups, &count);
+
+			for(i = 0; i < count; i++)
+				XPUSHs(sv_2mortal(newSViv(groups[i])));
+		}
+	}
+	/*
+	if (count == 0) {
+		XPUSHs(&PL_sv_undef);
+	}
+	*/
---
+++ lib/User/getgrouplist.pm
@@ -0,0 +1,48 @@
+# $Id: getgrouplist.pm,v 1.3 2009/10/20 08:06:41 bastian Exp $
+# Copyright (c) 2007-2009 Collax GmbH
+package User::getgrouplist;
+
+use 5.006001;
+use strict;
+
+require Exporter;
+require DynaLoader;
+
+our @ISA = qw(Exporter DynaLoader);
+our @EXPORT = qw ( getgrouplist );
+
+=head1 NAME
+
+User::getgrouplist - Interface to glibc "getgrouplist" function
+
+=head1 VERSION
+
+Version 1.0
+
+=cut
+
+our $VERSION = "1.0";
+
+=head1 SYNOPSIS
+
+    use User::getgrouplist;
+
+    my $username = "johndoe";
+    my @list = getgrouplist($username);
+    print("Groups for user $username: " . join(" ", @list));
+
+=head1 Description
+
+C<User::getgrouplist> is an interface for the C<getgrouplist> function
+available on many BSD-like Unix systems, and Linux. It returns the list of all
+groups a user is in.
+
+=head1 Exports
+
+Function C<getgrouplist($username)>
+
+=cut
+
+bootstrap User::getgrouplist;
+
+1;
